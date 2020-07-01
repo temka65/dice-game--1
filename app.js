c1b33ac0065bfebe7feb7bf23c3d3658740ec650
temka <temka65@yahoo.com>
2020-07-01 13:11:43 +0800

    eelj solih

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
-var activePlayer = 1;
+var activePlayer = 0;
 
 var scores = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.floor(Math.random()*6) + 1;
+var diceNumber = Math.floor(Math.random()*6) + 1;
 
-document.querySelector('#score-0').textContent = dice;
 
-document.querySelector('#score-1').textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
+// Shoog shideh event listener
+document.querySelector('.btn-roll').addEventListener("click", function() {
+    // 1-6 random too avah
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+   
+    // Shoonii zurag gargaj ireh
+    diceDom.style.display = "block";
 
-console.log('SHoo: ' + dice);
+    // buusan toonii zurag gargah
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+    // buusan toog nemne
+    if (diceNumber !== 1) {
+      roundScore = roundScore + diceNumber;
+      document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+
+      
+      document.getElementById("current-" + activePlayer).textContent = 0;       
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      diceDom.style.display = "none";
+      document.querySelector(".player-1-panel").classList.toggle("active");
+      roundScore = 0;
+
+    //   if (activePlayer === 0) {
+    //       activePlayer = 1;
+    //   } else {
+    //       activePlayer = 0;
+    //   }
+    }
+});
